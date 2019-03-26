27c46e878a05c75b27ba4ad14073c18b0bc381b6
S6203-1-08 <1355605105@qq.com>
2019-03-26 11:33:20 +0800

    3/26

--- src/main/java/com/lcvc/ebuy_maven_ssm/dao/AdminDao.java
+++ src/main/java/com/lcvc/ebuy_maven_ssm/dao/AdminDao.java
@@ -1,5 +1,7 @@
 package com.lcvc.ebuy_maven_ssm.dao;
 
+import com.lcvc.ebuy_maven_ssm.model.Admin;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -17,7 +19,7 @@ public class AdminDao {
 		PreparedStatement pstmt=null;
 		ResultSet rs=null;
 		try{
-			String sql="select * from admin where username=? and password=?";
+			String sql="select * from admin where username=? and password=? ";
 			pstmt=conn.prepareStatement(sql);
 			pstmt.setString(1, username);
 			pstmt.setString(2, password);
@@ -31,6 +33,31 @@ public class AdminDao {
 			DBHelper.closeConn(conn,pstmt,rs);
 		}
 		return judge;
+	}
+	public Admin GetAdmin(String username){
+		Admin admin=null;
+
+		Connection conn=DBHelper.getConnection();
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		try{
+			String sql="select * from admin where username=?  ";
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setString(1, username);
+			rs=pstmt.executeQuery();
+			if(rs.next()){//如果能够从数据库找到记录
+			admin=new Admin();
+			admin.setId(rs.getInt("id"));
+			admin.setName(rs.getString("name"));
+			admin.setUsername(rs.getString("username"));
+			admin.setPassword(rs.getString("password"));
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			DBHelper.closeConn(conn,pstmt,rs);
+		}
+		return admin;
 	}
 	
 	
